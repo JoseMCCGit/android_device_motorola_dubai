af49c664f872bcf1cc130941a1974a29f1367d1c
Marc Bourgoin <themard85@gmail.com>
2024-05-26 04:32:08 +0200

    dubai: Reorganization of DT structure and change dependencies
    Import sm7325-common resources to main device tree
    
    Change-Id: I4ac949607f6a7cd0f0baaa8f2b19b3f01733ae7d
    Signed-off-by: JoseMCCGit <josemccdev@proton.me>
    Co-authored-by: Marc Bourgoin <themard85@gmail.com>
    Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -9,7 +9,49 @@
 set -e
 
 export DEVICE=dubai
-export DEVICE_COMMON=sm7325-common
 export VENDOR=motorola
 
-"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
+set -e
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+ANDROID_ROOT="${MY_DIR}/../../.."
+
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+# Initialize the helper for common
+setup_vendor "${DEVICE}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true
+
+# Warning headers and guards
+write_headers "berlin berlna dubai xpeng"
+
+# The standard common blobs
+write_makefiles "${MY_DIR}/proprietary-files.txt" true
+
+# Finish
+write_footers
+
+if [ -s "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files.txt" ]; then
+    # Reinitialize the helper for device
+    setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false
+
+    # Warning headers and guards
+    write_headers
+
+    # The standard device blobs
+    write_makefiles "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files.txt" true
+
+    if [ -f "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-firmware.txt" ]; then
+        append_firmware_calls_to_makefiles "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-firmware.txt"
+    fi
+
+    # Finish
+    write_footers
+fi
